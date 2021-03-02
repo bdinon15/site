7d364a0d52dd687e4ae3bd33034a6f67f35d75a3
bdinon15 <ben.dinon15@gmail.com>
2021-03-02 17:18:56 -0500

    Major Site Update

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function myFunction() {
+  document.getElementById("email").innerHTML = "ben.dinon15@gmail.com";
+}
+function link() {
+  document.getElementById("email").innerHTML = "Email";
+}
+function msf (){
+		document.getElementById("meta").innerHTML = "msfconsole";
+}
+function meta (){
+		document.getElementById("meta").innerHTML = "Metasploit";
+}
+
+function clone () {
+	document.getElementById("git").innerHTML = "git clone";
+}
+
+function git () {
+	document.getElementById("git").innerHTML = "Git(Hub)";
+}
