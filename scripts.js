3509e448e34cb79a077b8411e31d2fe303bdddf1
bdinon15 <ben.dinon15@gmail.com>
2021-03-02 20:42:27 -0500

    update

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
 function myFunction() {
   document.getElementById("email").innerHTML = "ben.dinon15@gmail.com";
 }
+
 function link() {
   document.getElementById("email").innerHTML = "Email";
 }
+
 function msf (){
 		document.getElementById("meta").innerHTML = "msfconsole";
 }
+
 function meta (){
 		document.getElementById("meta").innerHTML = "Metasploit";
 }
@@ -17,4 +20,12 @@ function clone () {
 
 function git () {
 	document.getElementById("git").innerHTML = "Git(Hub)";
+}
+
+function bash() {
+	document.getElementById("bash").innerHTML = "#!/bin/bash";
+}
+
+function bin() {
+	document.getElementById("bash").innerHTML = "BASH";
 }
